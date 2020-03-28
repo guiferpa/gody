ab405bd16c7241fc7122a0898bb46bb07ad94896
Guilherme Paixão <guiferpa@gmail.com>
2020-03-28 00:59:48 -0300

    Create new contract for validator struct [#4]
    - Improve examples
    - Created many ways to validation
    - Validator struct
      it's important for a flexibility in future, let it easier
      configuration.

---
+++ examples/custom_rule.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/guiferpa/gody"
+	"github.com/guiferpa/gody/rule"
+)
+
+// ErrInvalidPalindrome is a custom error to a specific rule implementation
+type ErrInvalidPalindrome struct {
+	Value string
+}
+
+func (e *ErrInvalidPalindrome) Error() string {
+	return fmt.Sprintf("invalid palindrome: %s", e.Value)
+}
+
+// PalindromeRule is a struct that implements the Rule interface
+type PalindromeRule struct{}
+
+// Name is a func from the Rule contract
+func (r *PalindromeRule) Name() string {
+	return "palindrome"
+}
+
+// Validate is a func from the Rule contract
+func (r *PalindromeRule) Validate(f, v, p string) (bool, error) {
+	// TODO: The algorithm for palindrome validation
+	return true, &ErrInvalidPalindrome{Value: v}
+}
+
+func CustomRuleValidation() {
+	b := struct {
+		Text       string `json:"text" validate:"min_bound=5"`
+		Palindrome string `json:"palindrome" validate:"palindrome"`
+	}{
+		Text:       "test-text",
+		Palindrome: "test-palindrome",
+	}
+
+	customRules := []gody.Rule{
+		&PalindromeRule{},
+	}
+
+	valid, err := gody.DefaultValidate(b, customRules)
+	if err != nil {
+		if !valid {
+			log.Println("body do not validated", err)
+		}
+
+		switch err.(type) {
+		case *rule.ErrRequired:
+			log.Println("required error:", err)
+
+		case *ErrInvalidPalindrome:
+			log.Println("palindrome error:", err)
+		}
+	}
+}
---
+++ examples/simple.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"log"
+
+	"github.com/guiferpa/gody"
+	"github.com/guiferpa/gody/rule"
+)
+
+func SimpleDefaultValidation() {
+	b := struct {
+		Text string `json:"text" validate:"not_empty"`
+	}{}
+
+	valid, err := gody.DefaultValidate(b, nil)
+	if err != nil {
+		if !valid {
+			log.Println("body do not validated:", err)
+		}
+
+		switch err.(type) {
+		case *rule.ErrNotEmpty:
+			log.Println("not empty error:", err)
+
+		}
+	}
+}
+
+func SimplePureValidation() {
+	b := struct {
+		Text string `json:"text" validate:"not_empty"`
+	}{}
+
+	rules := []gody.Rule{
+		rule.NotEmpty,
+	}
+	valid, err := gody.Validate(b, rules)
+	if err != nil {
+		if !valid {
+			log.Println("body do not validated:", err)
+		}
+
+		switch err.(type) {
+		case *rule.ErrNotEmpty:
+			log.Println("not empty error:", err)
+
+		}
+	}
+}
+
+func SimpleValidationFromValidator() {
+	b := struct {
+		Text string `json:"text" validate:"not_empty"`
+	}{}
+
+	validator := gody.NewValidator()
+
+	rules := []gody.Rule{
+		rule.NotEmpty,
+	}
+	if err := validator.AddRules(rules); err != nil {
+		log.Println(err)
+		return
+	}
+
+	valid, err := validator.Validate(b)
+	if err != nil {
+		if !valid {
+			log.Println("body do not validated:", err)
+		}
+
+		switch err.(type) {
+		case *rule.ErrNotEmpty:
+			log.Println("not empty error:", err)
+
+		}
+	}
+}
