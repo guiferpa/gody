614b8edd0a58fbfbc4fc70caa2a51f9b5da5e579
isac <isacgrizante18@gmail.com>
2024-10-24 19:16:45 -0300

    fix: Test cases

--- rule/is_boolean_test.go
+++ rule/is_boolean_test.go
@@ -6,27 +6,43 @@ import (
 )
 
 func TestIsBooleanName(t *testing.T) {
-	got := NotEmpty.Name()
+	r := IsBoolean 
+	got := r.Name()
 	want := "is_boolean"
 	if got != want {
-		t.Error("IsBoolean.Name(), got: %v, want: %v", got, want)
+		t.Errorf("IsBoolean.Name(), got: %v, want: %v", got, want)
 	}
 }
 
 func TestIsBooleanWithSuccessful(t *testing.T) {
-	valid, err := NotEmpty.Validate("text", "true", "")
-	if got, want := valid, true; got != want {
-		t.Errorf(`valid, _ := IsBoolean.Validate("text", "true", ""), got: %v, want: %v`, got, want)
+	r := IsBoolean 
+	cases := [2]string{"true", "false"}
+	for _, test := range cases {
+		valid, err := r.Validate("text", test, "")
+		if got, want := valid, true; got != want {
+			t.Errorf(`valid, _ := IsBoolean.Validate("text", "true", ""), got: %v, want: %v`, got, want)
+		}
+		if got, want := reflect.TypeOf(err), reflect.TypeOf(nil); got != want {
+			t.Errorf(`_, err := IsBoolean.Validate("text", "test", ""), got: %v, want: %v`, got, want)
+		}
 	}
-	if got, want := reflect.TypeOf(err), reflect.TypeOf(nil); got != want {
-		t.Errorf(`_, err := IsBoolean.Validate("text", "test", ""), got: %v, want: %v`, got, want)
+}
+
+func TestIsBooleanWithInvalidValues(t *testing.T) {
+	r := IsBoolean 
+	ok, err := r.Validate("text", "james", "")
+	if err == nil {
+		t.Error("Unexpected no error")
+	}
+	if got, want := ok, true; got != want {
+		t.Errorf(`_, err := IsBoolean.Validate("text", "", ""), got: %v, want: %v`, got, want)
 	}
 }
 
 func TestIsBooleanError(t *testing.T) {
-	err := &ErrIsBoolean{field: "text"}
+	err := &ErrIsBoolean{Field: "text"}
 	got := err.Error()
-	want := "field text cannot be empty"
+	want := "field text must be boolean"
 	if got != want {
 		t.Errorf(`&ErrNotEmpty{Field: "text"}.Error(), got: %v, want: %v`, got, want)
 	}
